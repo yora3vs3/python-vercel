ce5086fc5ccf66e4b771bc781cbefabb43d7aadc
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2023-03-01 10:52:28 +0300

    invasion

--- api/index.py
+++ api/index.py
@@ -1,10 +1,47 @@
-from http.server import BaseHTTPRequestHandler
+import pygame
+from pygame.sprite import Group
+from settings import Settings   
+from game_stats import GameStatistics
+from scoreboard import Scoreboard
+from button import Button
+from ship import Ship
+import game_functions as gf
 
-class handler(BaseHTTPRequestHandler):
+def run_game():
+    #Initialize pygame, settings and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion by Walter")
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
+    play_button = Button(ai_settings, screen, "Play")
+    
+    # Create an instance to store game statistics and make a scoreboard
+    # Create an instance to store game settings
+    stats = GameStatistics(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+
+    # Make a ship, group of bullets and group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create a fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the mwain loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            bullets.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        
+run_game()
+   
---
+++ api/settings.py
@@ -0,0 +1,61 @@
+class Settings():
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the Game's static Settings."""
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 760
+        self.bg_color = (0, 0, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_speed_factor = 3
+        self.bullet_width = 1000
+        self.bullet_height = 1
+        self.bullet_color = 50, 60, 30
+        self.bullets_allowed = 10
+
+        # Alien settings
+        self.alien_speed_factor = 5
+        
+        # Creating settings for fleet direction
+        self.fleet_drop_speed = 10
+        
+        # Fleet direction of 1 rep right; -1 rep left
+        self.fleet_direction = 2
+
+        # How quick;y the game speeds up
+        self.speedup_scale = 1.1
+        
+        # How quickly the alien point values increase:
+        self.score_scale = 1.5
+        
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize setings that can change during the game"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # Fleet direction of 1 represents right; -1 rep left
+        self.fleet_direction = 1
+        
+        # Scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Icrease speed settings and alien point values"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+        
+
+
+        
+
